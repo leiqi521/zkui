cd629cd95555708740f6f2c5cfa01e182cb34b5e
895698283@qq.com <LeiQi219517>
2020-10-18 20:13:12 +0800

    基础框架

---
+++ src/main/java/com/leiqi/zk/zkui/ZkuiApplication.java
@@ -0,0 +1,13 @@
+package com.leiqi.zk.zkui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leiqi/zk/zkui/ZkuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leiqi.zk.zkui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

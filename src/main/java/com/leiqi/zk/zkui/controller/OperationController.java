1a74dba9df385533e48bd664b7f5e35d1234cd5a
895698283@qq.com <LeiQi219517>
2020-10-21 22:21:50 +0800

    节点添加、查询、修改、删除功能基本完成

--- src/main/java/com/leiqi/zk/zkui/controller/OperationController.java
+++ src/main/java/com/leiqi/zk/zkui/controller/OperationController.java
@@ -1,5 +1,6 @@
 package com.leiqi.zk.zkui.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.leiqi.zk.zkui.vo.LeafBean;
 import org.apache.curator.framework.CuratorFramework;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
@@ -39,7 +42,7 @@ public class OperationController {
                     nodeList.forEach(p -> {
                         try {
 
-                            String name = "/".equals(zkPath) ? zkPath : zkPath + "/" + p;
+                            String name = ("/".equals(zkPath) ? zkPath : zkPath + "/") + p;
                             List<String> cList = zkClient.getChildren().forPath(name);
                             //判断节点是不是孙子节点
                             if (cList.isEmpty()) {
@@ -81,20 +84,18 @@ public class OperationController {
         }
         return "home";
     }
+
     public final static String ROLE_ADMIN = "ROLE_ADMIN";
+
     @PostMapping("/home")
     public String home(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
         String action = request.getParameter("action");
         String currentPath = request.getParameter("currentPath");
         String displayPath = request.getParameter("displayPath");
         String newProperty = request.getParameter("newProperty");
         String newValue = request.getParameter("newValue");
         String newNode = request.getParameter("newNode");
-
         String[] nodeChkGroup = request.getParameterValues("nodeChkGroup");
-        String[] propChkGroup = request.getParameterValues("propChkGroup");
-
         String searchStr = request.getParameter("searchStr").trim();
         String authRole = authentication.getAuthorities().iterator().next().toString();
         switch (action) {
@@ -109,68 +110,85 @@ public class OperationController {
                     e.printStackTrace();
                 }
                 break;
-            case "Save Property":
-//                if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-//                    //Save the new node.
-//                    ZooKeeperUtil.INSTANCE.createNode(currentPath, newProperty, newValue, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-//                    request.getSession().setAttribute("flashMsg", "Property Saved!");
-//                    if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
-//                        newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
-//                    }
-//                    dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Saving Property: " + currentPath + "," + newProperty + "=" + newValue);
-//                }
-//                response.sendRedirect("/home?zkPath=" + displayPath);
-                break;
             case "Update Property":
-//                if (!newProperty.equals("") && !currentPath.equals("") && authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-//                    //Save the new node.
-//                    ZooKeeperUtil.INSTANCE.setPropertyValue(currentPath, newProperty, newValue, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-//                    request.getSession().setAttribute("flashMsg", "Property Updated!");
-//                    if (ZooKeeperUtil.INSTANCE.checkIfPwdField(newProperty)) {
-//                        newValue = ZooKeeperUtil.INSTANCE.SOPA_PIPA;
-//                    }
-//                    dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Updating Property: " + currentPath + "," + newProperty + "=" + newValue);
-//                }
-//                response.sendRedirect("/home?zkPath=" + displayPath);
+                if (!"".equals(newProperty) && !"".equals(currentPath) && ROLE_ADMIN.equals(authRole)) {
+                    //Save the new node.
+                    try {
+                        zkClient.setData().forPath(currentPath + newProperty, newValue.getBytes());
+                        response.sendRedirect("/home?zkPath=" + displayPath);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
             case "Search":
-//                Set<LeafBean> searchResult = ZooKeeperUtil.INSTANCE.searchTree(searchStr, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps), authRole);
-//                templateParam.put("searchResult", searchResult);
-//                ServletUtil.INSTANCE.renderHtml(request, response, templateParam, "search.ftl.html");
+                try {
+                    Set<LeafBean> searchResult = new TreeSet<>();
+                    setLeaf(searchStr, "/", searchResult);
+                    String re = JSONObject.toJSONString(searchResult);
+                    response.sendRedirect("/search?searchResult=" + URLEncoder.encode(re, StandardCharsets.UTF_8.name()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 break;
             case "Delete":
-//                if (authRole.equals(ZooKeeperUtil.ROLE_ADMIN)) {
-//
-//                    if (propChkGroup != null) {
-//                        for (String prop : propChkGroup) {
-//                            List delPropLst = Arrays.asList(prop);
-//                            ZooKeeperUtil.INSTANCE.deleteLeaves(delPropLst, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-//                            request.getSession().setAttribute("flashMsg", "Delete Completed!");
-//                            dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Deleting Property: " + delPropLst.toString());
-//                        }
-//                    }
-//                    if (nodeChkGroup != null) {
-//                        for (String node : nodeChkGroup) {
-//                            List delNodeLst = Arrays.asList(node);
-//                            ZooKeeperUtil.INSTANCE.deleteFolders(delNodeLst, ServletUtil.INSTANCE.getZookeeper(request, response, zkServerLst[0], globalProps));
-//                            request.getSession().setAttribute("flashMsg", "Delete Completed!");
-//                            dao.insertHistory((String) request.getSession().getAttribute("authName"), request.getRemoteAddr(), "Deleting Nodes: " + delNodeLst.toString());
-//                        }
-//                    }
-//
-//                }
-//                response.sendRedirect("/home?zkPath=" + displayPath);
+                if (ROLE_ADMIN.equals(authRole)) {
+
+                    if (nodeChkGroup != null) {
+                        for (String node : nodeChkGroup) {
+                            try {
+                                zkClient.delete().forPath(node);
+                                response.sendRedirect("/home?zkPath=" + displayPath);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
                 break;
             default:
-//                response.sendRedirect("/home");
+                try {
+                    response.sendRedirect("/home");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
-
-        return "1";
+        return "";
     }
 
     @GetMapping("/loginError")
     public String loginError(ModelMap map) {
         map.addAttribute("error", "用户名或密码错误");
         return "login_error";
+    }
+
+    @GetMapping("/search")
+    public String search(ModelMap map, @RequestParam(required = false, value = "searchResult") String searchResult) {
+        List<LeafBean> searchResult1 = JSONObject.parseArray(searchResult, LeafBean.class);
+        map.addAttribute("searchResult", searchResult1);
+        return "search";
+    }
+
+    private void setLeaf(String searchName, String path, Set<LeafBean> searchResult) {
+        try {
+            List<String> list = zkClient.getChildren().forPath(path);
+            if (list != null && !list.isEmpty()) {
+                List<String> filterList = list.stream().filter(x -> !"zookeeper".equals(x)).collect(Collectors.toList());
+                filterList.forEach(x -> {
+                    String p = ("/".equals(path) ? path : path + "/") + x;
+                    setLeaf(searchName, p, searchResult);
+                });
+            } else {
+                byte[] data = zkClient.getData().forPath(path);
+                String name = path.substring(path.lastIndexOf("/") + 1);
+                if (name.contains(searchName)) {
+                    LeafBean leafBean = new LeafBean(path, name, data);
+                    searchResult.add(leafBean);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
